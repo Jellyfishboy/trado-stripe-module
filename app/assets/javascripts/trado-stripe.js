c089234c9b1cfacd3de25f28026399e869b9c0b3
Tom Dallimore <me@tomdallimore.com>
2016-10-27 13:30:57 +0700

    Finished setting up the stripe integration with js validation.

--- app/assets/javascripts/trado-stripe.js
+++ app/assets/javascripts/trado-stripe.js
@@ -2,15 +2,25 @@ var order;
 
 jQuery(function() {
   Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
-  return order.setupForm();
+  order.setupForm();
 });
 
 order = {
   setupForm: function() {
-    return $('.process_order').submit(function() {
+    $('.process_order').submit(function() {
+      var $form = $(this);
       $('input[type=submit]').attr('disabled', true);
+      // Move to redlight theme
+      $('.stripe-error, .alert-stripe-checkout, .stripe-error-terms').remove();
+      $('#stripe_card_error').html('');
+      $('.field_with_errors').each(function()
+      {
+        $(this).find('input').unwrap();
+        $(this).find('select').unwrap();
+      });
+      // end
       if ($('#stripe_card_number').length) {
-        order.processCard();
+        order.validateForm($form);
         return false;
       } else {
         return true;
@@ -25,16 +35,62 @@ order = {
       expMonth: $('#stripe_card_month').val(),
       expYear: $('#stripe_card_year').val()
     };
-    return Stripe.createToken(card, order.handleStripeResponse);
+    Stripe.createToken(card, order.handleStripeResponse);
   },
   handleStripeResponse: function(status, response) {
     if (status === 200) {
       $('#order_stripe_card_token').val(response.id);
-      return $('.process_order')[0].submit();
+      $('.process_order')[0].submit();
     } else {
-      $('#stripe_error').text(response.error.message);
+      $('#stripe_card_error').text(response.error.message);
+      $('#stripe-form-fields input, #stripe-form-fields select').each(function()
+      {
+        $(this).wrap('<div class="field_with_errors"></div>');
+      });
+      $('.checkout-body').prepend('<div class="errors stripe-error"><p>Please complete all of the required fields</p></div>');
+      $('.checkout').prepend('<div class="alert alert-orange alert-stripe-checkout"><i class="icon-blocked"></i>An error ocurred with your order. Please try again.</div>');
+      $('input[type=submit]').attr('disabled', false);
       $('#checkoutLoadingModal').modal('hide');
-      return $('input[type=submit]').attr('disabled', false);
     }
+  },
+  validateForm: function(form)
+  {
+    $.ajax(
+    {
+      url: '/carts/stripe/confirm',
+      type: 'POST',
+      data: form.serialize(),
+      dataType: 'json',
+      success: function (data)
+      {
+        order.processCard();
+      },
+      error: function(xhr, evt, status)
+      {
+        order.jsonErrors(xhr, evt, status, form);
+      }
+    });
+  },
+  jsonErrors: function(xhr, evt, status, form)
+  {
+    var content, value, _i, _len, _ref, $this;
+    $this = form;
+    errorKeys = $.parseJSON(xhr.responseText).errors;
+    $('input[type=submit]').attr('disabled', false);
+    $('#checkoutLoadingModal').modal('hide');
+
+    // Move to redlight theme
+    $.each(errorKeys, function(index, value)
+    {
+      var $input = $('#' + value);
+      if (value === 'terms')
+      {
+        $('<span class="error-explanation stripe-error-terms">You must tick the box in order to place your order.</span>').insertBefore('input[type="hidden"][name="order[terms]"]');
+      }
+      $input.wrap('<div class="field_with_errors"></div>')
+    });
+    $('.checkout-body').prepend('<div class="errors stripe-error"><p>Please complete all of the required fields</p></div>');
+    $('.checkout').prepend('<div class="alert alert-orange alert-stripe-checkout"><i class="icon-blocked"></i>An error ocurred with your order. Please try again.</div>');
+    // end
   }
 };
--- app/controllers/trado_stripe_module/stripe_controller.rb
+++ app/controllers/trado_stripe_module/stripe_controller.rb
@@ -8,19 +8,33 @@ class TradoStripeModule::StripeController < ApplicationController
         set_grouped_countries
         set_browser_data
         @order.attributes = params[:order]
-        if @order.save
-            set_order_id_session
-            @order.remove_redundant_stripe_cards
-            @order.create_stripe_card
-            @order.calculate(current_cart, Store.tax_rate)
-            redirect_to confirm_order_url(@order)
-        else
-            flash_message :error, 'An error ocurred with your order. Please try again.'
-            render theme_presenter.page_template_path('carts/checkout'), layout: theme_presenter.layout_template_path
+        respond_to do |format|
+            format.html do
+                begin
+                    if @order.save
+                        set_order_id_session
+                        @order.remove_redundant_stripe_cards
+                        @order.create_stripe_card
+                        @order.calculate(current_cart, Store.tax_rate)
+                        redirect_to confirm_order_url(@order)
+                    else
+                        flash_message :error, 'An error ocurred with your order. Please try again.'
+                        render theme_presenter.page_template_path('carts/checkout'), layout: theme_presenter.layout_template_path
+                    end
+                rescue Stripe::InvalidRequestError, Stripe::APIConnectionError => e
+                    flash_message :error, 'An error ocurred with your order. Please try again.'  
+                    Rails.logger.error "Stripe Error: #{e} ? #{@order.email} | #{@order.id}"
+                    redirect_to checkout_carts_url
+                end
+            end
+
+            format.json do
+                if @order.valid?
+                    render json: { }, status: 200
+                else
+                    render json: { errors: @order.errors.keys.map{|e| e.to_s.split('.').join('_') } }, status: 422
+                end
+            end
         end
-    rescue Stripe::InvalidRequestError, Stripe::APIConnectionError => e
-        flash_message :error, 'An error ocurred with your order. Please try again.'  
-        Rails.logger.error "Stripe Error: #{e} ? #{@order.email} | #{@order.id}"
-        redirect_to checkout_carts_url
     end
 end
