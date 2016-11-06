113b3edb2f72eff025c3a7a22a26002894a8e71b
Tom Dallimore <me@tomdallimore.com>
2016-11-06 19:44:45 +0700

    Fixed bug whereby Order was not being associated with current Cart when Stripe::CardError was being raised. Refactored rescue statements in StripeController to ensure order data was being persisted.

--- app/controllers/trado_stripe_module/stripe_controller.rb
+++ app/controllers/trado_stripe_module/stripe_controller.rb
@@ -13,18 +13,18 @@ class TradoStripeModule::StripeController < ApplicationController
                 begin
                     if @order.save
                         set_order_id_session
+                        @order.calculate(current_cart, Store.tax_rate)
                         @order.remove_redundant_stripe_cards
                         @order.create_stripe_card
-                        @order.calculate(current_cart, Store.tax_rate)
                         redirect_to confirm_order_url(@order)
                     else
                         flash_message :error, 'An error ocurred with your order. Please try again.'
                         render theme_presenter.page_template_path('carts/checkout'), layout: theme_presenter.layout_template_path
                     end
-                rescue Stripe::InvalidRequestError, Stripe::APIConnectionError => e
-                    flash_message :error, 'An error ocurred with your order. Please try again.'  
+                rescue Stripe::InvalidRequestError, Stripe::APIConnectionError, Stripe::CardError, NoMethodError => e
+                    flash_message :error, 'An error ocurred with your order, please confirm your card details and try again.'  
                     Rails.logger.error "Stripe Error: #{e} ? #{@order.email} | #{@order.id}"
-                    redirect_to checkout_carts_url
+                    render theme_presenter.page_template_path('carts/checkout'), layout: theme_presenter.layout_template_path
                 end
             end
 
